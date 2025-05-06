47f3b03b773e799122688b7cf2d8214d47c54ab0
Vivor1212 <syhlerp@yahoo.dk>
2025-05-06 11:48:41 +0200

    test

--- Parkeringsplads/Program.cs
+++ Parkeringsplads/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<ITripService, EFTripService>();
 builder.Services.AddDbContext<ParkeringspladsContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Simply")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Simplytest")));
 
 var app = builder.Build();
 
